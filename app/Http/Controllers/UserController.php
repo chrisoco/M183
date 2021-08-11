24aa6cdbb7855185ee6af862b945b276108475a3
Chris <christopher.oconnor@santismail.ch>
2021-08-11 12:22:16 +0200

    Sortierung News nach created_at DESC

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         return view('index', [
-            'news' => News::all(),
+            'news' => News::all()->sortByDesc('created_at'),
         ]);
     }
 
